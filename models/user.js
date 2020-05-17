a1ced0b9e94a01be8521f82f4211ffefba0bb7d4
AnkurSaini <ankursainidun@gmail.com>
2020-05-17 13:17:46 +0530

    Add model files

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose  = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email : String,
+    image : String,
+    posts : [
+        {
+            type: Schema.Types.ObjectId, 
+            ref: 'Post' 
+        }
+    ]
+});
+
+UserSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', UserSchema);
+
+/*
+User 
+  - email - string
+  - password - string 
+  - username - string
+  - image - string
+  - posts - array of objects ref post 
+*/  
